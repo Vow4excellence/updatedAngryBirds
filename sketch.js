c38690f82e63ca1c2d50c7e1a3278662c7d4c048
Shreya <Shreyachopra.2012@gmail.com>
2020-10-31 22:11:22 +0530

    added platform to game

--- sketch.js
+++ sketch.js
@@ -16,8 +16,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
-
+    ground = new Ground(600,height,1200,20);
+   platform = new Ground(150, 325,300, 150);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -45,6 +45,7 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+    platform.display();
     box1.display();
     box2.display();
     ground.display();
